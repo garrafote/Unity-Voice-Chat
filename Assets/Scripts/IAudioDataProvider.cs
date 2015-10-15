2034160cb79ec625e98d41f2e9861235dc2415e5
Lucas Vasconcelos <garrafote@gmail.com>
2015-10-15 01:42:54 -0400

    Ported to unity 5

--- Assets/Scripts/IAudioDataProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿/// <summary>
-/// Represents a type that provides audio frame data.
-/// </summary>
-public interface IAudioDataProvider
-{
-    event AudioDataReceivedEventHandler AudioDataReceived;
-}
-
-public delegate void AudioDataReceivedEventHandler(AudioFrameData frameData);
